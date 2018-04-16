d9d887015f94abd28f76784c341ed13853f8a0ee
BUDDHAPLEX <ericandrade@gmail.com>
2018-04-16 18:23:33 -0400

    finished assignment and first stretch goal.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,11 +7,12 @@ import { INCREMENT, DECREMENT } from '../actions';
 // need to do with the count in each case?
 export default (count = 0, action) => {
   switch (action.type) {
-    case INCREMENT:
-    // Fill in the body of this case
-    case DECREMENT:
-    // Fill in the body of this case
-    default:
-      return count;
-  }
-};
+    case INCREMENT: 
+      return count + 1;
+
+    case DECREMENT: 
+      return count - 1;
+    
+    default: 
+      return count;} 
+}
